2d9719f059f0e3613fe04c235f5140f306275171
Chris Hurst <46472228+ytsruh@users.noreply.github.com>
2023-06-25 12:07:26 +0100

    add SveleKit & Skeleton UI

---
+++ client/src/routes/+layout.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	// The ordering of these imports is critical to your app working properly
+	import '@skeletonlabs/skeleton/themes/theme-crimson.css';
+	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	// Most of your app wide CSS should be put in this file
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import { LightSwitch } from '@skeletonlabs/skeleton';
+	import FaSearchengin from 'svelte-icons/fa/FaSearchengin.svelte';
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead"><div class="icon"><FaSearchengin /></div></svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a class="btn btn-sm variant-ghost-tertiary" href="/"> Home </a>
+				<a class="btn btn-sm variant-ghost-tertiary" href="/stats"> Stats </a>
+				<LightSwitch />
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
+
+<style>
+	.icon {
+		width: 24px;
+		height: 24px;
+	}
+</style>
---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	const random = Math.random().toString().substring(0, 5).split('.')[1];
+	function clear() {
+		const field = <HTMLInputElement>document.getElementById('input');
+		field.value = '';
+	}
+</script>
+
+<div class="flex flex-col h-full items-center justify-center px-5">
+	<div
+		class="flex flex-col justify-center items-center space-y-5 pb-10 text-center px-10 break-words"
+	>
+		<h1 class="text-3xl md:text-5xl">
+			Search 0.0000000{random}%<span class="text-lg">*</span> of the Web
+		</h1>
+		<p class="text-sm">* completely made up statistic</p>
+	</div>
+	<form on:submit|preventDefault class="w-full sm:w-2/3 lg:w-1/2 xl:w-1/3">
+		<input id="input" class="input" type="text" placeholder="Search" />
+		<div class="flex justify-around py-5">
+			<button type="button" class="btn variant-filled-primary">Search</button>
+			<button on:click={clear} class="btn variant-filled-tertiary">Clear</button>
+		</div>
+	</form>
+</div>
