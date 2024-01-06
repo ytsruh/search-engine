164d7d9c2bc1adcddcef14c6aa6b138fa441be24
Chris Hurst <46472228+ytsruh@users.noreply.github.com>
2024-01-06 16:33:13 +0000

    removed api routes & setup templates

--- go.mod
+++ go.mod
@@ -4,25 +4,19 @@ go 1.20
 
 require (
 	github.com/gofiber/fiber/v2 v2.47.0
-	github.com/golang-jwt/jwt/v5 v5.0.0
-	github.com/google/uuid v1.3.0
+	github.com/gofiber/template/html/v2 v2.0.5
 	github.com/joho/godotenv v1.5.1
 	github.com/robfig/cron/v3 v3.0.1
 	github.com/shopspring/decimal v1.3.1
 	github.com/steebchen/prisma-client-go v0.31.5
-	golang.org/x/crypto v0.10.0
 	golang.org/x/net v0.11.0
-	gorm.io/driver/postgres v1.5.2
-	gorm.io/gorm v1.25.1
 )
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
-	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
-	github.com/jackc/pgx/v5 v5.3.1 // indirect
-	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/gofiber/template v1.8.2 // indirect
+	github.com/gofiber/utils v1.1.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/klauspost/compress v1.16.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
@@ -36,5 +30,4 @@ require (
 	github.com/valyala/fasthttp v1.48.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.9.0 // indirect
-	golang.org/x/text v0.10.0 // indirect
 )
--- routes/index.go
+++ routes/index.go
@@ -8,7 +8,6 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cache"
 	"github.com/gofiber/fiber/v2/middleware/monitor"
-	"github.com/golang-jwt/jwt/v5"
 )
 
 var SecretKey = os.Getenv("SECRET_KEY")
@@ -28,47 +27,11 @@ var setCache = cache.New(cache.Config{
 	CacheControl: true,
 })
 
-func SetRoutes(app *fiber.App) {
-	api := app.Group("/api") // Routes located at : /api
-	api.Get("/metrics", monitor.New(monitor.Config{Title: "Live Server Metrics"}))
-	api.Post("/search", saveQueryMiddleware, runSearch)
-	api.Post("/crawl", manualCrawl)
-	api.Get("/stats", setCache, getStats)
-
-	auth := api.Group("/auth") // Routes located at : /api/auth
-	auth.Post("/register", registerUser)
-	auth.Post("/login", loginUser)
-	auth.Post("/logout", logoutUser)
-
-	admin := api.Group("/admin") // Routes located at : /api/admin
-	admin.Use(func(c *fiber.Ctx) error {
-		// Get the cookie by name
-		cookie := c.Cookies("search-engine")
-		// Parse the cookie & check for errors
-		token, err := jwt.ParseWithClaims(cookie, &CustomClaims{}, func(token *jwt.Token) (interface{}, error) {
-			return []byte(SecretKey), nil
-		})
-		if err != nil {
-			c.Status(fiber.StatusUnauthorized)
-			return c.JSON(fiber.Map{
-				"message": "unauthenticated",
-			})
-		}
-		// Parse the custom claims & check jwt is valid
-		claims, ok := token.Claims.(*CustomClaims)
-		if ok && token.Valid {
-			c.Locals("user", claims)
-			return c.Next()
-		}
-		// Return unauthorized if jwt is not valid
-		c.Status(fiber.StatusUnauthorized)
-		return c.JSON(fiber.Map{
-			"message": "unauthenticated",
+func SetAPIRoutes(app *fiber.App) {
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("index", fiber.Map{
+			"PageTitle": "Welcome to the ytsruh search engine",
 		})
 	})
-	admin.Get("/me", getUser)
-	admin.Post("/create", manuallyCreateUrl)
-	admin.Get("/settings", getSettings)
-	admin.Put("/settings", updateSettings)
-
+	app.Get("/metrics", monitor.New(monitor.Config{Title: "Live Server Metrics"}))
 }
